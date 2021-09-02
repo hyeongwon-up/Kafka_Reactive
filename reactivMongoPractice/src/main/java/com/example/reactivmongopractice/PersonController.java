4761fe0066e5d523677a942de3f1c106bec6fe99
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-02 19:28:20 +0900

    reactive CRUD

--- reactivMongoPractice/src/main/java/com/example/reactivmongopractice/PersonController.java
+++ reactivMongoPractice/src/main/java/com/example/reactivmongopractice/PersonController.java
@@ -19,8 +19,8 @@ public class PersonController {
         return personRepository.findAll();
     }
 
-    @GetMapping(value = "/stream" , produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Person> streamAllPerson(){
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Person> streamAllPerson() {
         return personRepository.findAll();
     }
 
@@ -33,8 +33,30 @@ public class PersonController {
         ).defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @PostMapping("/{id}")
+    public Mono<ResponseEntity<Person>> updatePerson(@PathVariable(value = "id") String id, @RequestBody Person person) {
+        return personRepository.findById(id)
+                .flatMap(
+                        it -> {
+                            it.setName(person.getName());
+                            it.setEmail(person.getEmail());
+                            return personRepository.save(it);
+                        }
+                ).map(
+                        res -> {
+                            return ResponseEntity.ok(res);
+                        }
+                ).defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 
-
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Void>> deletePerson(@PathVariable(value = "id") String id) {
+        return personRepository.findById(id)
+                .flatMap(
+                        it -> personRepository.delete(it)
+                                .then(Mono.just(new ResponseEntity<Void>(HttpStatus.OK)))
+                ).defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 
 
 }
